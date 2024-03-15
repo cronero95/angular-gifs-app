4fba9d22f34b4d848668b3554c938da72800e072
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-15 13:59:31 -0500

    Use @ViewChild to get a local reference.
    We have an input element with local reference '#txtTagInput', so we use '@ViewChild('txtTagInput')' in order to associate the element with a property in the '.ts' file.

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
@@ -8,9 +8,19 @@ import { Component } from '@angular/core';
       type="text"
       class="form-control"
       placeholder="Search for Gifs..."
+      (keyup.enter)="searchTag()"
+      #txtTagInput
     >
   `
 })
 export class SearchBoxComponent {
 
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  searchTag(): void {
+    const newTag = this.tagInput.nativeElement.value;
+
+    console.log({ newTag });
+  }
 }
