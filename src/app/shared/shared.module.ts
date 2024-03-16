0cf004340a54eb8e77ad41a72ef6a6bf49795bb0
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-16 16:39:20 -0500

    Show loading image while Gif is loading.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    LazyImageComponent
   ],
   exports: [
-    SidebarComponent
+    SidebarComponent,
+    LazyImageComponent
   ],
   imports: [
     CommonModule
